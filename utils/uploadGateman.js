b03c81953e1dbaa7b9552496f1a17caec14226ea
nitinthakurdev <radheradhe9870023@gmail.com>
2025-11-03 12:03:14 +0530

    Refactor: comment out quality-check route and improve upload directory handling

--- utils/uploadGateman.js
+++ utils/uploadGateman.js
@@ -1,10 +1,19 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+// Folder path
+const uploadDir = path.join(__dirname, "../uploads/gateman");
+
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // Storage engine
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/gateman/"); // store inside uploads/gateman/
+    cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
     const uniqueName = Date.now() + "-" + file.originalname;
